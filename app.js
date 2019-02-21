bcc081ba720dd8f113d9f76e548e90b2e63b7a69
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-02-21 20:11:49 +0100

    added chron

--- app.js
+++ app.js
@@ -1,41 +1,57 @@
 import { app } from 'mu';
 import { getUnprocessedPublishedResources, updateStatus, PENDING_STATUS, SUCCESS_STATUS, FAILED_STATUS } from './support/queries';
 import { startPipeline } from './support/pipeline';
+import { CronJob } from 'cron';
 
 const PENDING_TIMEOUT = process.env.PENDING_TIMEOUT_HOURS || 3;
+const CRON_FREQUENCY = process.env.CACHING_CRON_PATTERN || '0 */5 * * * *';
+//TODO: chron jobs, further testing, notulen linken
+
+new CronJob(CRON_FREQUENCY, async function() {
+  console.log(`Service triggered by cron job at ${new Date().toISOString()}`);
+  try {
+    await startPublishing();
+  } catch (err) {
+    console.log("We had a bonobo");
+    console.log(err);
+  }
+}, null, true);
+
+async function startPublishing(){
+  let unprocessedResources = await getUnprocessedPublishedResources(PENDING_TIMEOUT);
+
+  console.log('Found ${unprocessedResources.length} to proces');
+
+  //lock resources (yes yes should be batch operation)
+  for(const item of unprocessedResources){
+    console.log(`-- Locking resources: ${item.resource}`);
+    item.numberOfRetries = (item.numberOfRetries || 0) + 1;
+    await updateStatus(item, PENDING_STATUS, );
+  }
+
+  for (const item of unprocessedResources) {
+    console.log(`-- Start processing: ${item.resource}`);
+
+    try {
+      await startPipeline(item);
+      await updateStatus(item, SUCCESS_STATUS, item.numberOfRetries);
+    }
+
+    catch(e){
+      console.log(`--Error processing: ${item.resource}`);
+      console.log(e);
+      await updateStatus(item, FAILED_STATUS, item.numberOfRetries);
+    }
+  }
+}
 
 /**
  * Starts extracting the published resources.
  */
 app.post('/publish-tasks', async function(req, res) {
   try {
-
-    let unprocessedResources = await getUnprocessedPublishedResources(PENDING_TIMEOUT);
-
-    //lock resources (yes yes should be batch operation)
-    for(const item of unprocessedResources){
-      console.log(`-- Locking resources: ${item.resource}`);
-      item.numberOfRetries = (item.numberOfRetries || 0) + 1;
-      await updateStatus(item, PENDING_STATUS, );
-    }
-
-    for (const item of unprocessedResources) {
-      console.log(`-- Start processing: ${item.resource}`);
-
-      try {
-        await startPipeline(item);
-        await updateStatus(item, SUCCESS_STATUS, item.numberOfRetries);
-      }
-
-      catch(e){
-        console.log(`--Error processing: ${item.resource}`);
-        console.log(e);
-        await updateStatus(item, FAILED_STATUS, item.numberOfRetries);
-      }
-    }
-
+    await startPublishing();
     res.send( { success: true } );
-
   } catch (err) {
     console.log("We had a bonobo");
     console.log(err);
@@ -44,4 +60,4 @@ app.post('/publish-tasks', async function(req, res) {
       .send( { message: `An error occurred while publishing`,
                err: JSON.stringify(err) } );
   }
-} );
+});
