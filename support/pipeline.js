f5fb231f49b5ce82c5c947443f33c17a402a8662
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-03-28 17:38:16 +0100

    simplified uri check
    it seems to consume a lot of resources

--- support/pipeline.js
+++ support/pipeline.js
@@ -307,15 +307,7 @@ function flatTriples(node){
  * checks if URI (stolen from SO)
  */
 function isURI(str) {
-  //Fuck this: see https://stackoverflow.com/a/45567717/1092608
-  var pattern = new RegExp('^((ft|htt)ps?:\\/\\/)?'+ // protocol
-  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name and extension
-  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-  '(\\:\\d+)?'+ // port
-  '(\\/[-a-z\\d%@_.~+&:]*)*'+ // path
-  '(\\?[;&a-z\\d%@_.,~+&:=-]*)?'+ // query string
-  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-  return pattern.test(str);
+  return /^https?:\/\/(.*)/.test(str);
 }
 
 /*
